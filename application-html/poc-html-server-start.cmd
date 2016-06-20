b7fbccc734f054823429cad246d57c39a723458e
Marc J. Andersen <mja@statgroup.dk>
2016-06-20 19:25:02 +0200

    Start scripts change to their own directory

--- application-html/poc-html-server-start.cmd
+++ application-html/poc-html-server-start.cmd
@@ -1,1 +1,10 @@
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+REM Get path for the script
+Set ScriptInvocation=%0
+Set ScriptName=%~n0
+Set ScriptFullPath=%~f0
+Set ScriptPath=%~p0
+
+cd/d %ScriptPath%
 c:\Python34\python.exe -m http.server
--- res-ttl/poc-fuseki-server.cmd
+++ res-ttl/poc-fuseki-server.cmd
@@ -1,6 +1,14 @@
+SETLOCAL ENABLEDELAYEDEXPANSION
+REM Get path for the script
+Set ScriptInvocation=%0
+Set ScriptName=%~n0
+Set ScriptFullPath=%~f0
+Set ScriptPath=%~p0
+
 SET FUSEKI_HOME=c:\opt\apache-jena-fuseki-2.0.0
 SET FUSEKI_BASE=c:\opt\apache-jena-fuseki-2.0.0
 
+cd/d %ScriptPath%
 java -Xmx1200M -cp %FUSEKI_HOME%\fuseki-server.jar org.apache.jena.fuseki.cmd.FusekiCmd --config=poc-fuseki-config.ttl
 
 
